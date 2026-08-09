0ae5efbeb2ea53070e027bd0ef58e442549a346f
dimon4iksc <119966135+dimon4iksc@users.noreply.github.com>
2026-08-09 11:32:52 +0300

    аі

--- src/js/services.js
+++ src/js/services.js
@@ -6,7 +6,7 @@ $(".btn-service").on("click", function() {
 	if (a[0] == 1) {
 		$("#servicesModalLabel").text("Полірування кузова авто");
 		$("#servicesModal .modal-body").append('<p><strong>CarLux Detailing</strong> - це сучасна детейлінг студія із досвідом роботи понад 5 років.' +
-			'<p><strong>Якість послуг</strong> гарантована нашим багаторічним досвідом роботи, та дотриманням європейських стандартів виконання.</p>' +
+			'<p><strong>Якість послуг</strong> гарантована нашим багаторічним досвідом роботи, та дотриманням європейських стандартів виконання.</p>' +
 			'<p>Детальніше про CarLux Detailing:</p>' +
 			'<p><strong>Наша робота</strong></p>' +
 			'<li>щотижня виконуємо більше 25 комплексів замовлень з хімчистки та полірування авто</li>' +
@@ -16,7 +16,7 @@ $(".btn-service").on("click", function() {
 			'<li>Використовуємо тільки професійну техніку та хімічні засоби від провідних світових виробників</li>' +
 			'<p><strong>Увага!</strong> Ми використовуємо виключно гіпоалергенну хімію, що не містить шкіливих для здоров’я речовин.</p>' +
 			'</p>&nbsp;</p>' +
-			'<div class="fm text_box"><table align="center" border="1" cellpadding="5" cellspacing="0" dir="ltr" style="width:100%;"><colgroup><col /><col /></colgroup><tbody><tr><td style="text-align: center;"><strong>Категорія авто</strong></td><td style="text-align: center;"><strong>Вартість, грн</strong></td></tr><tr><td style="text-align: center;">Mini</td><td style="text-align: center;">6900</td></tr><tr><td style="text-align: center;">Sedan</td><td style="text-align: center;">7900</td></tr><tr><td style="text-align: center;">Crossover</td><td style="text-align: center;">8900</td></tr><tr><td style="text-align: center;">Jeep</td><td style="text-align: center;">9900</td></tr></tbody></table></div>');
+			'<div class="fm text_box"><table align="center" border="1" cellpadding="5" cellspacing="0" dir="ltr" style="width:100%;"><colgroup><col /><col /><col /></colgroup><tbody><tr><td style="text-align: center;"><strong>Категорія авто</strong></td><td style="text-align: center;"><strong>Полірування, $</strong></td><td style="text-align: center;"><strong>Кераміка, $</strong></td></tr><tr><td style="text-align: center;">Mini</td><td style="text-align: center;">150</td><td style="text-align: center;">150</td></tr><tr><td style="text-align: center;">Sedan</td><td style="text-align: center;">180</td><td style="text-align: center;">200</td></tr><tr><td style="text-align: center;">Crossover</td><td style="text-align: center;">200</td><td style="text-align: center;">250</td></tr><tr><td style="text-align: center;">Jeep</td><td style="text-align: center;">220</td><td style="text-align: center;">300</td></tr></tbody></table></div>');
 	} else if (a[0] == 2) {
 		$("#servicesModalLabel").text("Хімчистка салону авто");
 		$("#servicesModal .modal-body").append('<strong>Хімчистка авто|CarLux</strong> - це сучасна технологія хімчистки яка передбачає використання професійної техніки та безпечних для навколішнього серидовища хімічних засобів вироблених у Німеччині та Італії. ' +
@@ -29,7 +29,7 @@ $(".btn-service").on("click", function() {
 			'<li><strong>3. Результат</strong> – салон авто сухий та чистий, плями видаленні, запах відсутній.</li>' +
 			'<li><strong>4. Повна мийка авто у подарунок.</strong></li>' +
 			'<p>&nbsp;</p>' +
-			'<div class="fm text_box"><table align="center" border="1" cellpadding="5" cellspacing="0" dir="ltr" style="width:100%;"><colgroup><col /><col /></colgroup><tbody><tr><td style="text-align: center;"><strong>Категорія авто</strong></td><td style="text-align: center;"><strong>Вартість, грн</strong></td></tr><tr><td style="text-align: center;">Mini</td><td style="text-align: center;">5000</td></tr><tr><td style="text-align: center;">Sedan</td><td style="text-align: center;">6000</td></tr><tr><td style="text-align: center;">Crossover</td><td style="text-align: center;">7000</td></tr><tr><td style="text-align: center;">Jeep</td><td style="text-align: center;">8000</td></tr></tbody></table></div>');
+			'<div class="fm text_box"><table align="center" border="1" cellpadding="5" cellspacing="0" dir="ltr" style="width:100%;"><colgroup><col /><col /><col /></colgroup><tbody><tr><td style="text-align: center;"><strong>Категорія авто</strong></td><td style="text-align: center;"><strong>Хімчистка Delux, грн</strong></td><td style="text-align: center;"><strong>Хімчистка Delux Premium, грн</strong></td></tr><tr><td style="text-align: center;">Mini</td><td style="text-align: center;">5000</td><td style="text-align: center;">6000</td></tr><tr><td style="text-align: center;">Sedan</td><td style="text-align: center;">6000</td><td style="text-align: center;">7000</td></tr><tr><td style="text-align: center;">Crossover</td><td style="text-align: center;">7000</td><td style="text-align: center;">8000</td></tr><tr><td style="text-align: center;">Jeep</td><td style="text-align: center;">8000</td><td style="text-align: center;">9000</td></tr></tbody></table></div>');
 	} else {
 		console.log("Error. Please, try again.")
 	}
